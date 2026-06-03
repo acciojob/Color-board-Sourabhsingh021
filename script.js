98b863c4d7ff66f6180e4b67c508f7f9c8163b1d
Sourabh Singh <67355597+Sourabhsingh021@users.noreply.github.com>
2026-06-03 16:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 const container = document.querySelector(".container");
 
 for (let i = 0; i < 800; i++) {
-    const box = document.createElement("div");
-    box.classList.add("square");
-    container.appendChild(box);
+  const box = document.createElement("div");
+  box.classList.add("square");
+  container.appendChild(box);
 
-    box.addEventListener("mouseenter", () => {
-        const color = getRandomColor();
+  const changeColor = () => {
+    const color = getRandomColor();
+    box.style.backgroundColor = color;
+    box.style.boxShadow = `0 0 15px ${color}`;
 
-        box.style.backgroundColor = color;
-        box.style.boxShadow = `0 0 15px ${color}`;
+    setTimeout(() => {
+      box.style.backgroundColor = "#1d1d1d";
+      box.style.boxShadow = "none";
+    }, 1000);
+  };
 
-        setTimeout(() => {
-            box.style.backgroundColor = "#1d1d1d";
-            box.style.boxShadow = "none";
-        }, 1000);
-    });
+  box.addEventListener("mouseover", changeColor);
 }
 
 function getRandomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-
-    return color;
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
